3c5d48ddec4edaf0751b8a6d37e9ceefb2dc122b
Johan Del Valle <43177845+JohanDelValleV@users.noreply.github.com>
2020-03-28 15:38:42 -0600

    Final sin graficas

--- images.py
+++ images.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import Entry, PhotoImage, Tk, filedialog, messagebox, ttk
 from tkinter.ttk import Button, Label
 import math
+import copy
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,6 +12,7 @@ from skimage.color import rgb2gray
 from skimage.feature import ORB, blob_doh, match_descriptors, plot_matches
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform, rotate
+from matplotlib.patches import ConnectionPatch
 
 pathIn = ''
 interface = Tk()
@@ -19,6 +21,8 @@ yPosition = ttk.Entry(interface)
 scale = ttk.Entry(interface)
 angle = ttk.Entry(interface)
 matches_array = []
+keypoints_original_blob = []
+keypoints_transformed_blob = []
 
 
 def openFile():
@@ -32,67 +36,131 @@ def openFile():
 
 def moveImage():
     global pathIn
-    matches_array = []
-    coincidence = []
     position_x = int(xPosition.get())
     positions = [(0, position_x), (position_x, position_x), (position_x, 0), (position_x, -position_x),
                  (0, -position_x), (-position_x, -position_x), (-position_x, 0), (-position_x, position_x)]
-    img = skimage.io.imread(pathIn)
     moved_cords = []
+    img = skimage.io.imread(pathIn)
+    num_rows_original, num_cols_original = img.shape[:2]
 
-    num_rows, num_cols = img.shape[:2]
+    img_origin = cv2.copyMakeBorder(
+        img, position_x, position_x, position_x, position_x, cv2.BORDER_CONSTANT, value=None)
+
+    new_num_rows, new_num_cols = img_origin.shape[:2]
     for i, z in positions:
         translation_matrix = np.float32(
             [[1, 0, i], [0, 1, z]])
         img_translation = cv2.warpAffine(
-            img, translation_matrix, (num_cols + 110, num_rows + 110))
+            img_origin, translation_matrix, (new_num_cols, new_num_rows))
 
-        matches, keypoints_original, keypoints_transformada = mainProcess(
-            img_translation)
+        keypoints_original, keypoints_transformada = mainProcess(img_origin,
+                                                                 img_translation)
         for blobs in keypoints_original:
             y, x, r = blobs
-            moved_cords.append((x+z, y+i))
+            moved_cords.append(((x+i, y+z), (x, y)))
 
-        compare_coords(keypoints_transformada, moved_cords)
+        compare_coords(keypoints_transformada, keypoints_original,
+                       moved_cords, img_origin, img_translation)
         moved_cords.clear()
-
-        coincidence.append(len(params(matches, keypoints_original)))
-        matches_array.append((len(matches)/len(keypoints_original))*100)
-    # print(matches_array, coincidence)
-
-    plt.title('Transformación de imagen (DoH)')
-    plt.xlabel('Transformación')
-    plt.ylabel('Porcentaje (%)')
-    plt.plot(matches_array,
-             color="blue", label="No. coincidencias")
-    plt.legend()
-    plt.show()
+    print('TERMINO')
+    graph(position_x, matches_array)
+    matches_array.clear()
 
 
-def compare_coords(keypoints, new_coords):
-    keypoints_x_y = []
+def compare_coords(keypoints_transformed, keypoints_originales, kp_by_me, original_image, transformed_image):
+    global matches_array
+    keypoints_transformed_x_y = []
+    keypoints_original_list = []
     matches = []
-    for blobs in keypoints:
-        y, x, r = blobs
-        keypoints_x_y.append((x, y))
 
-    for kp_cords in [list(a) for a in keypoints_x_y]:
-        best_match = 5
-        for kp_trans in [list(b) for b in new_coords]:
+    for blobs in keypoints_transformed:
+        y, x, r = blobs
+        keypoints_transformed_x_y.append((x, y))
+
+    for xy in keypoints_originales:
+        y, x, r = xy
+        keypoints_original_list.append((x, y))
+
+    keypoints_transformed_list = [list(a) for a in keypoints_transformed_x_y]
+    keypoints_by_me = [list(b) for b in kp_by_me]
+    print(f'TRANSFORMED-------> {keypoints_transformed_list}')
+    print(f'BY ME-------> {kp_by_me}\n')
+
+    for kp_trans, kp_origin in keypoints_by_me:
+        best_match = 10
+        for kp_cords in keypoints_transformed_list:
             result = math.sqrt(
                 pow((kp_trans[0]-kp_cords[0]), 2) + pow((kp_trans[1]-kp_cords[1]), 2))
             if result < best_match:
                 best_match = result
-        if best_match <= 3 or best_match >= 3:
-            matches.append(best_match)
+        if best_match <= 4:
+            matches.append((kp_trans, kp_origin))
     print(f'Matches: {matches}')
+    draw_matches(keypoints_transformed_x_y, matches, keypoints_by_me,
+                 original_image, transformed_image)
+    matches_array.append(params(matches, keypoints_original_list))
+    print(matches_array)
+    return True
+
+
+def graph(transformation, matches):
+    plt.title('DoH')
+    plt.xlabel('Grados de transformación')
+    plt.ylabel('Porcentaje (%)')
+    plt.plot(transformation, matches_array,
+             color="blue", label="No. coincidencias")
+    plt.xticks(transformation)
+    plt.legend()
+    plt.show()
+
+
+def draw_matches(keypoints_transformed, matches, kp_by_me, original_image, transformed_image):
+    global keypoints_original_blob
+    keypoints_original_x_y = []
+    img_copy = copy.copy(transformed_image)
+
+    for blobs in keypoints_original_blob:
+        y, x, r = blobs
+        keypoints_original_x_y.append((x, y))
+
+    for x in keypoints_original_x_y:
+        cv2.circle(original_image, (int(x[0]), int(x[1])), 3, (255, 0, 0), -1)
+
+    cv2.imshow('Imagen original', original_image)
+    cv2.waitKey()
+
+    for x2 in keypoints_transformed:
+        cv2.circle(transformed_image,
+                   (int(x2[0]), int(x2[1])), 3, (255, 255, 0), -1)
+
+    cv2.imshow('Imagen desplazada', transformed_image)
+    cv2.waitKey()
+
+    for x3, x4 in kp_by_me:
+        cv2.circle(img_copy, (int(x3[0]), int(x3[1])), 3, (0, 128, 0), -1)
+
+    cv2.imshow('Imagen a kp_by_me', img_copy)
+    cv2.waitKey()
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.imshow(original_image)
+    ax2.imshow(img_copy)
+    ax1.set_axis_off()
+    ax2.set_axis_off()
+    coordsA = "data"
+    coordsB = "data"
+
+    for i, z in matches:
+        con = ConnectionPatch(xyA=i, xyB=z, coordsA=coordsA, coordsB=coordsB,
+                              axesA=ax2, axesB=ax1, arrowstyle="-", color="red")
+        ax2.add_artist(con)
+    # plt.tight_layout()
+    plt.show()
 
 
 def resizeImage():
     global pathIn
-    coincidence = []
     size_array = [400, 200, 50, 25]
-    matches_array = []
     resized_cords = []
     img = cv2.imread(pathIn)
 
@@ -102,154 +170,86 @@ def resizeImage():
         dim = (width, height)
         resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
-        matches, keypoints_original, keypoints_transformada = mainProcess(
-            resized)
+        keypoints_original, keypoints_transformada = mainProcess(img,
+                                                                 resized)
         for blobs in keypoints_original:
             y, x, r = blobs
-            resized_cords.append(((x*(i/100)), (y*(i/100))))
+            resized_cords.append((((x*(i/100)), (y*(i/100))), (x, y)))
         print(f'Resized cords: {resized_cords}')
-        # compare_coords(keypoints_transformada, resized_cords)
-
+        compare_coords(keypoints_transformada, keypoints_original,
+                       resized_cords, img, resized)
         resized_cords.clear()
 
-        coincidence.append(len(params(matches, keypoints_original)))
-        matches_array.append((len(matches)/len(keypoints_original))*100)
-    # print(size_array, matches_array, coincidence)
 
-    plt.title('Escalado de imagen (DoH)')
-    plt.xlabel('Tamaño de transformación')
-    plt.ylabel('Porcentaje (%)')
-    plt.plot(size_array, matches_array,
-             color="blue", label="No. coincidencias")
-    plt.xticks(size_array)
-    plt.legend()
-    plt.show()
-
-
-def rotate_kp(p, origin=(0, 0), degrees=0):
-    angle = np.deg2rad(degrees)
-    R = np.array([[np.cos(angle), -np.sin(angle)],
-                  [np.sin(angle),  np.cos(angle)]])
-    o = np.atleast_2d(origin)
-    p = np.atleast_2d(p)
-    return np.squeeze((R @ (p.T-o.T) + o.T).T)
-
-
-def rotate_around_point_lowperf(point, radians, origin=(0, 0)):
+def rotate_kp(point, radians, origin=(0, 0)):
     x, y = point
     ox, oy = origin
 
     qx = ox + math.cos(radians) * (x - ox) + math.sin(radians) * (y - oy)
     qy = oy + -math.sin(radians) * (x - ox) + math.cos(radians) * (y - oy)
 
-    return qx, qy
+    return (qx, qy)
 
 
 def rotateImage():
     global pathIn
-    matches_array = []
     grades_array = []
-    coincidence = []
-    coords = []
+    kp_by_me = []
+
     img = skimage.io.imread(pathIn)
     num_rows_original, num_cols_original = img.shape[:2]
-    num_rows_original = num_rows_original/2
-    num_cols_original = num_cols_original/2
+
+    hip = math.sqrt(pow(num_rows_original, 2) + pow(num_cols_original, 2))//2
+
+    img_origin = cv2.copyMakeBorder(img, int(hip-(num_cols_original//2)), int(hip-(num_rows_original//2)), int(hip-(
+        num_cols_original//2)), int(hip-(num_rows_original//2)), cv2.BORDER_CONSTANT, value=None)
+
+    new_num_rows_original, new_num_cols_original = img_origin.shape[:2]
+    new_num_cols_original = new_num_cols_original/2
+    new_num_rows_original = new_num_rows_original/2
 
     for i in range(int(angle.get()), 360, int(angle.get())):
         grades_array.append(i)
-        image_center = rotate(img, i, resize=True)
-        num_rows_rotated, num_cols_rotated = image_center.shape[:2]
-        num_rows_rotated = num_rows_rotated/2
+        image_center = rotate(img_origin, i, resize=False)
+        num_cols_rotated, num_rows_rotated = image_center.shape[:2]
         num_cols_rotated = num_cols_rotated/2
+        num_rows_rotated = num_rows_rotated/2
 
-        cv2.imshow('Image', image_center)
-        cv2.waitKey(0)
-
-        matches, keypoints_original, keypoints_transformada = mainProcess(
-            image_center)
+        keypoints_original, keypoints_transformada = mainProcess(
+            img_origin, image_center)
         print(f'Angulo: {i}')
         for blobs in keypoints_original:
             y, x, r = blobs
-            # print(rotate_kp((x, y),
-            #                 origin=(num_rows_original+(num_rows_original - num_rows_rotated),
-            #                         num_cols_original+(num_cols_original-num_cols_rotated)), degrees=i))
-            print(rotate_around_point_lowperf((x, y), math.radians(i), origin=(num_cols_original+(num_cols_original - num_cols_rotated),
-                                                                               num_rows_original+(num_rows_original-num_rows_rotated))))
-        # coords.clear()
-        coincidence.append(len(params(matches, keypoints_original)))
-        matches_array.append((len(matches)/len(keypoints_original))*100)
-    # print(grades_array, matches_array, coincidence)
-
-    plt.title('Rotación de imagen (DoH)')
-    plt.xlabel('Grados de transformación')
-    plt.ylabel('Porcentaje (%)')
-    plt.plot(grades_array, matches_array,
-             color="blue", label="No. coincidencias")
-    plt.xticks(grades_array)
-    plt.legend()
-    plt.show()
+            kp_by_me.append((rotate_kp((x, y), math.radians(i), origin=(new_num_cols_original,
+                                                                        new_num_rows_original)), (x, y)))
+        print(f'Juntos: {kp_by_me}')
+        compare_coords(keypoints_transformada,
+                       keypoints_original, kp_by_me, img_origin, image_center)
+        kp_by_me.clear()
 
 
-def procesar():
+def mainProcess(image_original, image_transformed):
     global pathIn
-    image = skimage.io.imread(pathIn)
-    image_gray = rgb2gray(image)
-
-    blobs_doh = blob_doh(image_gray, min_sigma=1, max_sigma=30,
-                         num_sigma=10, threshold=0.01, overlap=0.5, log_scale=False)
-    fig, grafica = plt.subplots(1)
-
-    grafica.set_title("DoH")
-    grafica.imshow(image)
-
-    for blob in blobs_doh:
-        y, x, r = blob
-        c = plt.Circle((x, y), r, color="red", linewidth=2, fill=False)
-        grafica.add_patch(c)
-    grafica.set_axis_off()
-    plt.show()
-
-
-def mainProcess(image):
-    global pathIn
-
-    image_gray = rgb2gray(image)
+    global keypoints_original_blob
+    global keypoints_transformed_blob
+    image_gray = rgb2gray(image_transformed)
     img_path_original = skimage.io.imread(pathIn)
-    img_original = rgb2gray(img_path_original)
+    img_original = rgb2gray(image_original)
 
     blobs_doh = blob_doh(img_original, min_sigma=1, max_sigma=30,
                          num_sigma=10, threshold=0.01, overlap=0.5, log_scale=False)
-    # print(f'Imagen original: {blobs_doh}')
+    keypoints_original_blob = blobs_doh
 
     blobs_doh2 = blob_doh(image_gray, min_sigma=1, max_sigma=30,
                           num_sigma=10, threshold=0.01, overlap=0.5, log_scale=False)
-    print(f'Imagen transformada: {blobs_doh2}')
+    keypoints_transformed_blob = blobs_doh2
 
-    descriptor_extractor = ORB(n_keypoints=len(blobs_doh))
-
-    descriptor_extractor.detect_and_extract(img_original)
-    # keypoints1 = descriptor_extractor.keypoints
-    descriptors1 = descriptor_extractor.descriptors
-
-    descriptor_extractor.detect_and_extract(image_gray)
-    # keypoints2 = descriptor_extractor.keypoints
-    descriptors2 = descriptor_extractor.descriptors
-
-    # fig, ax = plt.subplots(1)
-    matches = match_descriptors(
-        descriptors1, descriptors2, cross_check=True, p=2, max_ratio=0.8)
-    # plot_matches(ax, img_path_original, image, blobs_doh, blobs_doh2, matches)
-
-    return matches, blobs_doh, blobs_doh2
+    return blobs_doh, blobs_doh2
 
 
 def params(matches, keypoints):
-    matches_list = []
-    for i in matches:
-        result = ((len(matches)*100))/len(keypoints)
-        matches_list.append(result)
-    return matches_list
+    result = ((len(matches)*100))/len(keypoints)
+    return result
 
 
 photo = PhotoImage(file=r"btn.png")
